f30fb56b49930dd4a15fd28196093e30d634862b
Johan <147413295+jkrangh@users.noreply.github.com>
2024-11-23 10:36:39 +0100

    Take turns changing background with CPU.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,19 +17,54 @@ namespace WpfTicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool gameOver;
+        List<Button> gameTiles = new List<Button>();
+        Random random = new Random();
+        int playerWins = 0;
+        int cpuWins = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
         }
 
-        private void btn1_Click(object sender, RoutedEventArgs e)
+        private void NewGame()
         {
+            gameTiles.Clear();
+            gameTiles.AddRange([btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9]);
 
+            foreach (Button button in gameTiles) 
+            {
+                button.Background = Brushes.White;
+            }
+            gameOver = false;
         }
+
+        private void PlayerMove(object sender, RoutedEventArgs e)
+        {            
+            var button = (Button)sender;
+            button.Background = Brushes.MediumPurple;
+            gameTiles.Remove(button);
+
+            
+            CpuMove();
+        }
+
+        private void CpuMove()
+        {
+            Thread.Sleep(500); // Adds suspense
+            int index = random.Next(gameTiles.Count);            
+            var button = gameTiles[index];
+            button.Background = Brushes.Orange;
+            gameTiles.Remove(button);
+        }
+
+
 
         private void btnNewGame_Click(object sender, RoutedEventArgs e)
         {
-
+            NewGame();
         }
     }
 }
