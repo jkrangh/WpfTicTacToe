7c76ca70116bad83c019b5f203bbfaf11feda750
Johan <147413295+jkrangh@users.noreply.github.com>
2024-11-23 12:25:55 +0100

    Winning conditions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace WpfTicTacToe
         bool gameOver;
         List<Button> gameTiles = new List<Button>();
         Random random = new Random();
+        CurrentPlayer player;
         int playerWins = 0;
         int cpuWins = 0;
 
@@ -41,6 +42,10 @@ namespace WpfTicTacToe
             }
             gameOver = false;
         }
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            NewGame();
+        }
 
         private void PlayerMove(object sender, RoutedEventArgs e)
         { 
@@ -51,33 +56,61 @@ namespace WpfTicTacToe
                 return;
             }
             else 
-            { 
+            {
+                player = CurrentPlayer.Player;
                 button.Background = Brushes.MediumPurple;
                 gameTiles.Remove(button);
-                CheckWin();
+                CheckWin(button);
                 CpuMove();
             }
         }
 
-        private void CheckWin()
-        {
-            throw new NotImplementedException();
-        }
 
         private void CpuMove()
         {
+            player = CurrentPlayer.CPU;
             Thread.Sleep(500); // Adds suspense
             int index = random.Next(gameTiles.Count);            
             var button = gameTiles[index];
             button.Background = Brushes.Orange;
             gameTiles.Remove(button);
+            CheckWin(button);
+
         }
 
-
-
-        private void btnNewGame_Click(object sender, RoutedEventArgs e)
+        
+        /*
+         * 1|2|3
+         * 4|5|6
+         * 7|8|9
+         */
+        private void CheckWin(Button button)
         {
-            NewGame();
+            //This is so ugly
+            {
+                if ((((button.Background == btn1.Background) && (button.Background == btn2.Background) && (button.Background == btn3.Background)) || //Row win
+                    ((button.Background == btn4.Background) && (button.Background == btn5.Background) && (button.Background == btn6.Background)) || //Row win
+                    ((button.Background == btn7.Background) && (button.Background == btn8.Background) && (button.Background == btn9.Background)))|| //Row win
+                    (((button.Background == btn1.Background) && (button.Background == btn4.Background) && (button.Background == btn7.Background)) || //Column win
+                    ((button.Background == btn2.Background) && (button.Background == btn5.Background) && (button.Background == btn8.Background)) || //Column win
+                    ((button.Background == btn3.Background) && (button.Background == btn6.Background) && (button.Background == btn9.Background))) || //Column win
+                    ((button.Background == btn1.Background) && (button.Background == btn5.Background) && (button.Background == btn9.Background)) || //Diagonal win
+                    ((button.Background == btn3.Background) && (button.Background == btn5.Background) && (button.Background == btn7.Background))) //Diagonal win
+                {
+                    gameOver = true;
+                    MessageBox.Show($"{player} Wins!");
+                    
+                    if (player == CurrentPlayer.Player)
+                        playerWins++;
+                    else
+                        cpuWins++;    
+                }
+            }
+
+            
         }
+
+
+
     }
 }
